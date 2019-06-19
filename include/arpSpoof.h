7fc3258d21198abe8a6bb0c2f943b243d8404431
seungho <seung7642@naver.com>
2019-06-19 02:51:35 -0700

    over all Framework

--- include/arpSpoof.h
+++ include/arpSpoof.h
@@ -5,9 +5,10 @@ class ArpSpoof
 		// method
 		ArpSpoof(u_char& interface, arpHeader arp, etherHeader ether);
 		~ArpSpoof();
-		u_char* getMacAddress();
-		int setArpHeader(u_char& senderMacAddress, char& sendIp, char& targetIp, arpHeader& arp);
-		int setEthHeader(u_char& senderMacAddress, etherHeader& eth);
+		int setSenderMacAddress();
+		u_char* setTargetMacAddress();
+		int setArpHeader(char& senderIpAddress, char& targetIpAddress);
+		int setEtherHeader();
 		int sendInfectPacket(pcap_t& handle, char& interface, char& senderIpAddress, char& targetIpAddress);
 		int receivePacketRelay();
 
--- src/arpSpoof.cpp
+++ src/arpSpoof.cpp
@@ -11,25 +11,62 @@ ArpSpoof::~ArpSpoof()
 	// do nothing
 }
 
+int
+ArpSpoof::setSenderMacAddress()
+{
+	// getting Sender MAC Address through ioctl().
+	struct ifreq ifr;
+	
+	int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+	if (sock == -1) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	strncpy(ifr.ifr_name, this->interface, IFNAMSIZ - 1);
+
+	if (ioctl(sock, SIOCGIFCONF, &ifr)) {
+		perror("ioctl");
+		exit(EXIT_FAILURE);
+	}
+
+	memset(this->senderMacAddress, ifr.ifr_hwaddr.sa_data, 6);
+
+	close(sock);
+	return 0;
+}
+
 u_char*
-ArpSpoof::getMacAddress(u_char* ip) 
+ArpSpoof::setTargetMacAddress(pcap_t* handle, u_char& senderIpAddress, u_char& senderMacAddress, u_char& targetIpAddress, u_char& targetMacAddress) 
 {
 	// getting MAC Address through Broadcasting.
 	
 	
+
 	return mac;	
 }
 
 int
-ArpSpoof::setArpHeader(u_char& senderMacAddress, char& sendIp, char& targetIp, arpHeader arp)
+ArpSpoof::setArpHeader(char& senderIpAddress, char& targetIpAddress)
 {
+	memset(this->arpHdr.senderHardwareAddress, this->senderMacAddress, 6);
+	memset(this->arpHdr.targetHardwareAddress, this->targetMacAddress, 6);
+	memset(this->arpHdr.senderProtocolAddress, senderIpAddress, 4);
+	memset(this->arpHdr.targetProtocolAddress, targetIpAddress, 4);
 
+	this->arpHdr.hardwareType = htons(ARP_HW_TYPE);
+	this->arpHdr.protocolType = htons(ARP_PROTO_TYPE);
+	this->arpHdr.hardwareLength = ARP_HW_LEN;
+	this->arpHdr.protocolLength = ARP_PROTO_LEN;
+	this->arpHdr.opcode = htons(ARP_OPCODE);
 }
 
 int
-ArpSpoof::setEtherHeader(u_char& senderMacAddress, etherHeader eth)
+ArpSpoof::setEtherHeader()
 {
-
+	memset(this->etherHdr.destinationMacAddress, this->targetMacAddress, 6);
+	memset(this->etherHdr.sourceMacAddress, this->senderMacAddress, 6);
+	this->etherHdr.type = htons(ETHERTYPE_ARP);
 }
 	
 int
